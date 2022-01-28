2cd3bcc462d1eab134e72484a4844f1a3401d34c
md-sahil-11 <mdsahilalam11@gmail.com>
2022-01-28 16:24:55 +0530

    settings

--- hemkunt_transport/settings.py
+++ hemkunt_transport/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '2zjl^7=l@#@rz&y9gf1+tkwvax5d2tlfh5mb-8(rjx!ws23n6w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
